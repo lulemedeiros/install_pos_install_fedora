69afbfdf90568aa5c16d235c9704021dca36330e
Luciano Leonel de Medeiros <lulu.medeiros@gmail.com>
2020-06-01 22:29:11 -0300

    Atualizando o instala_geral.sh
    Adicionei alguns comentários já deixei público.

--- instala_geral.sh
+++ instala_geral.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Atualmente uso Fedora 32 64 Cinnamon
+
 set -xueo pipefail
 
 sudo dnf -y install fedora-workstation-repositories
 
 sudo dnf update -y
 
+# Instalando os programas que eu gosto, para trabalho e diversão.
 sudo dnf -y install clamav clamtk
 sudo freshclam
 sudo dnf config-manager --set-enabled google-chrome
@@ -13,7 +16,6 @@ sudo dnf -y install google-chrome-stable
 sudo dnf -y install numlockx.x86_64
 sudo dnf -y install remmina
 sudo dnf -y install xpad.x86_64
-sudo dnf -y install htop.x86_64
 sudo dnf -y install git
 sudo dnf -y install sl
 sudo dnf -y install gimp
@@ -27,6 +29,8 @@ sudo dnf -y install asciiquarium.noarch
 sudo dnf -y install xrdp
 sudo dnf -y install xorgxrdp
 sudo dnf -y install bashtop
+sudo dnf -y install htop.x86_64
+sudo dnf -y install ytop.x86_64
 sudo dnf -y install filezilla.x86_64
 sudo dnf -y install figlet.x86_64
 sudo dnf -y install tilix.x86_64
@@ -42,7 +46,14 @@ sudo dnf -y copr enable daniruiz/flat-remix
 
 sudo dnf update -y
 
-# instalando o Plank corretamente
+# Instalando o Plank corretamente
+# Renomeando o repositório do fedora pois o plank que tem lá
+# não aceita a configuração de zoom dos icones.
+# E instalando o plank do repositório do copr
+# depois de instalado eu volto o repositório do fedora
+# mas, adiciono a exclusão de atualização do plank
+# no dnf.conf evidando que ele atualize pelo repo
+# da fedora e desconfigure tudo.
 sudo mv /etc/yum.repos.d/fedora.repo fedora.repo.bkp
 sudo dnf -y install plank.x86_64
 sudo dnf -y install plank-docklets.x86_64
@@ -64,13 +75,16 @@ sudo dnf -y install @virtualization
 sudo dnf -y install virt-manager bridge-utils libvirt virt-install qemu-kvm
 sudo dnf -y install virt-top libguestfs-tools
 
+# Instalei esse novo browser para testar, ainda não tenho opinião sobre ele, tem propaganda dele mar barra as dos sites.
 sudo dnf -y config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 sudo dnf -y install brave-keyring brave-browser
 
+# Instalando o programas para acessar as maquinas
 wget https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm -O teamviewer.rpm
 sudo dnf -y install teamviewer.rpm
 
+# Instalando meus modulos basicos de python
 sudo dnf -y install python3.x86_64 \
     python-crypto \
     python-devel \
@@ -88,11 +102,10 @@ sudo dnf -y install python3.x86_64 \
     python3-ldap3.noarch \
     python-vlc \
 
-sudo systemctl start xrdp
-sudo systemctl enable xrdp
-
-sudo systemctl start libvirtd
-sudo systemctl enable libvirtd
+# Instalando e configurando minha área de trabalho para acessar de fora
+# não se angane, não é só isso, há muito mais segredos nesta configuração rsrs
+sudo systemctl start xrdp libvirtd
+sudo systemctl enable xrdp libvirtd
 
 sudo firewall-cmd --add-port=3389/tcp --permanent
 sudo firewall-cmd --reload
