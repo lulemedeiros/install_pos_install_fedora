b5cd825b85da584138e6d1ca1e1b87e9f2951f6d
Luciano Leonel de Medeiros <lulu.medeiros@gmail.com>
2022-11-29 11:44:02 -0300

    Update instala_geral.sh

--- instala_geral.sh
+++ instala_geral.sh
@@ -27,6 +27,7 @@ sudo dnf -y install gpick.x86_64
 sudo dnf -y install tilix.x86_64
 sudo dnf -y install gnome-software.x86_64
 sudo dnf -y install libreoffice-langpack-pt-BR.x86_64
+sudo dnf -y install ulauncher 
 sudo dnf -y install libreoffice-icon-theme-papirus.noarch
 #sudo rpm --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
 #sudo printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=gitlab.com_paulcarroty_vscodium_repo\nbaseurl=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg" |sudo tee -a /etc/yum.repos.d/vscodium.repo
